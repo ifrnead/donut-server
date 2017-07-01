f19c766cfcd70164e1c16f8a46afbefa7f583a07
Jalerson Lima <jalerson@gmail.com>
2017-07-01 12:57:53 -0300

    Implementação dos testes automatizados.

--- spec/controllers/api/rooms_spec.rb
+++ spec/controllers/api/rooms_spec.rb
@@ -16,11 +16,33 @@ RSpec.describe "Rooms API", type: :request do
     expect(JSON.parse(response.body).size).to eq(@user.rooms.size)
   end
 
+  it "retrieves room details" do
+    room = @user.rooms.first
+    get "/api/rooms/#{room.id}", headers: @headers
+
+    expect(response).to be_success
+    expect(response.content_type).to eq "application/json"
+    expect(response.body).to eq(room.to_json)
+  end
+
+  it "tries to retrieve nonexistent room details" do
+    get "/api/rooms/9999999999", headers: @headers
+
+    expect(response).to be_not_found
+  end
+
+  it "tries to retrieve room details which the user isn't included" do
+    room = create(:room)
+    get "/api/rooms/#{room.id}", headers: @headers
+
+    expect(response).to be_unauthorized
+  end
+
   it "try to use the API without token" do
     get '/api/rooms'
     expect(response).to be_unauthorized
 
-    get "/api/rooms/#{room.id}"
+    get "/api/rooms/#{Room.first.id}"
     expect(response).to be_unauthorized
   end
 
