5d515927a0a75419c7392a4b5fdb9d11b9b6a7b2
Jalerson Lima <jalerson@gmail.com>
2017-09-27 11:08:39 -0300

    Removendo o índice de unicidade de email.

---
+++ db/migrate/20170927140112_remove_uniqueness_email.rb
@@ -0,0 +1,5 @@
+class RemoveUniquenessEmail < ActiveRecord::Migration[5.1]
+  def change
+    remove_index :users, :email
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616140645) do
+ActiveRecord::Schema.define(version: 20170927140112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,7 +69,6 @@ ActiveRecord::Schema.define(version: 20170616140645) do
     t.string "token"
     t.string "password_salt"
     t.index ["current_suap_token"], name: "index_users_on_current_suap_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["token"], name: "index_users_on_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
